1e7fc1b24474fac4e3867ed8c1e29a3ba92ae9a4
Matthias Lehmann <mlehmann83@gmail.com>
2018-05-16 21:38:10 +0200

    LCD display

--- rs485_subscriber/src/main.cpp
+++ rs485_subscriber/src/main.cpp
@@ -1,10 +1,14 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include "LiquidCrystal_I2C.h"
 
 #define RS485Transmit    HIGH
 #define RS485Receive     LOW
 
 SoftwareSerial mySerial(10, 11);
+
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 struct SensorData {
     int hum;
@@ -16,6 +20,7 @@ void setup() {
     digitalWrite(3, LOW);
     Serial.begin(9600);
     mySerial.begin(9600);
+    lcd.begin();
 }
 
 byte buffer[4];
@@ -25,10 +30,18 @@ void loop() {
         while(mySerial.peek() > -1){
             mySerial.readBytes((uint8_t *) &sensor_data, sizeof(sensor_data));
         }
-        
+
         Serial.print("Temp:");
         Serial.print(sensor_data.temp);
         Serial.print(" Humidity:");
         Serial.println(sensor_data.hum);
+
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Temp: ");
+        lcd.print(sensor_data.temp);
+        lcd.setCursor(0, 1);
+        lcd.print("Humidity: ");
+        lcd.print(sensor_data.hum);
     }
 }
